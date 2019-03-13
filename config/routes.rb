142bdecced306851d1fda07847bb08b61753b64d
JoeyYuas <joeyyuasa@gmail.com>
2019-03-13 22:45:56 +0900

    Profileページと作品集ページを作成。ヘッダーを作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "pages#profile"
+  get "art" => "pages#art"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
